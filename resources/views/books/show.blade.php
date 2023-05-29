f660b56a9a0c8b717e74324c22e1d965454830c7
unknown <rvillareal@uas.com.ph>
2023-05-29 18:55:44 +0800

    refactor: adds hyphen in div class

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -4,10 +4,10 @@
 
 <div class="row">
     <div class="col-lg-12 margin tb">
-        <div class="pull left">
+        <div class="pull-left">
             <h2>Show book</h2>
         </div>
-        <div class="pull right">
+        <div class="pull-right">
             <a class="btn btn-primary"href="{{ route('books.index') }}">Back</a>
         </div>
     </div>
