5db128a4abfd0da1d6ddd73d1c7e5d82409d97d8
unknown <rvillareal@uas.com.ph>
2023-05-30 18:06:33 +0800

    feat: add login credentials

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,62 +1,64 @@
-@extends('books.layout')
+@extends('layouts.app')
 
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="pull-left">
-                <h2>Book Application</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-success" href="{{ route('books.create') }}"> Add New Book</a>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="pull-left">
+                    <h2>Book Application</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-success" href="{{ route('books.create') }}"> Add New Book</a>
+                </div>
             </div>
         </div>
-    </div>
-
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
-
     
-
-    <table class="table table-bordered">
-        <tr>
-            <th width="80px">@sortablelink('id')</th>
-            <th>@sortablelink('name')</th>
-            <th>@sortablelink('author')</th>
-            <th width="280px">Action</th>
-        </tr>
-
-        @if ($books->count() == 0)
-        <tr>
-            <td colspan="5">No books to display.</td>
-        </tr>
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>
+            </div>
         @endif
-
-        @foreach ($books as $key => $book)
-        <tr>
-            <td>{{ $book->id }}</td>
-            <td>{{ $book->name }}</td>
-            <td>{{ $book->author }}</td>
-            <td>
-                <form action="{{ route('books.destroy',$book->id) }}" method="POST">
-                    <a class="btn btn-info" href="{{ route('books.show',$book->id) }}">Show</a>
-                    <a class="btn btn-primary" href="{{ route('books.edit',$book->id) }}">Edit</a>
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-
-    </table>
-    {{ $books->links() }}
-    <p>
-        Displaying {{$books->count()}} of {{ $books->total() }} book(s).
-    </p>
+    
+        
+    
+        <table class="table table-bordered">
+            <tr>
+                <th width="80px">@sortablelink('id')</th>
+                <th>@sortablelink('name')</th>
+                <th>@sortablelink('author')</th>
+                <th width="280px">Action</th>
+            </tr>
+    
+            @if ($books->count() == 0)
+            <tr>
+                <td colspan="5">No books to display.</td>
+            </tr>
+            @endif
+    
+            @foreach ($books as $key => $book)
+            <tr>
+                <td>{{ $book->id }}</td>
+                <td>{{ $book->name }}</td>
+                <td>{{ $book->author }}</td>
+                <td>
+                    <form action="{{ route('books.destroy',$book->id) }}" method="POST">
+                        <a class="btn btn-info" href="{{ route('books.show',$book->id) }}">Show</a>
+                        <a class="btn btn-primary" href="{{ route('books.edit',$book->id) }}">Edit</a>
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+    
+        </table>
+        {{ $books->links() }}
+        <p>
+            Displaying {{$books->count()}} of {{ $books->total() }} book(s).
+        </p>
+    </div>
 
 @endsection
--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,27 +1,29 @@
-@extends('books.layout')
+@extends('layouts.app')
 
 @section('content')
 
-<div class="row">
-    <div class="col-lg-12 margin tb">
-        <div class="pull-left">
-            <h2>Show book</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary"href="{{ route('books.index') }}">Back</a>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 margin tb">
+            <div class="pull-left">
+                <h2>Show book</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary"href="{{ route('books.index') }}">Back</a>
+            </div>
         </div>
     </div>
-</div>
-
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form group">
-            <strong>Name:</strong>
-            {{ $book->name }}
-        </div>
-        <div class="form group">
-            <strong>Author:</strong>
-            {{ $book->author }}
+    
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form group">
+                <strong>Name:</strong>
+                {{ $book->name }}
+            </div>
+            <div class="form group">
+                <strong>Author:</strong>
+                {{ $book->author }}
+            </div>
         </div>
     </div>
 </div>
